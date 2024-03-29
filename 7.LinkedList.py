db040d9cf2feba649a75b5617b57abf511c45869
nithisha09 <nithishakarthik.09@gmail.com>
2024-03-29 21:31:20 +0530

    Linked List basics

--- 7.LinkedList.py
+++ 7.LinkedList.py
@@ -3,5 +3,34 @@ class Node:
         self.value = value
         self.next = None
 
-new_node = Node(10)
-print(new_node.value)
+class LinkedList:
+    # def __init__(self):
+    #     self.head = None
+    #     self.tail = None
+    #     self.length = 0
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+my_linked_list = LinkedList(9)
+print(my_linked_list.head.value)
+my_linked_list.append(15)
+my_linked_list.append(16)
+my_linked_list.append(20)
+print(my_linked_list.head.value)
+print(my_linked_list.tail.value)
+print(my_linked_list.length)
+
